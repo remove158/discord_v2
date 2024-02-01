c46a8d83823c3fd875282ee1cf1e47c7bf7f3a1b
remove158 <remove158@gmail.com>
2024-02-02 00:31:55 +0700

    add: gemini interface

--- src/commands/gemini.ts
+++ src/commands/gemini.ts
@@ -1,33 +1,31 @@
-import { SlashCommandBuilder, type Interaction, type CacheType, Client, EmbedBuilder } from "discord.js"
+import { SlashCommandBuilder } from "discord.js"
 import { model } from "../utils/gemini"
-import type { Manager } from "erela.js"
-import { builder, silentMessage } from "../utils/message"
+import {  silentMessage } from "../utils/message"
+import { Command } from "@/types/Client"
 
 
 
-const handler = {
+export default {
 	data: new SlashCommandBuilder()
 		.setName("gemini")
 		.setDescription('Try gemini Pro!')
 		.addStringOption(option =>
 			option.setName('prompt')
 				.setDescription('Input your prompt').setRequired(true))
-
 	,
-	async execute(interaction: Interaction<CacheType>, client: Client, manager : Manager) {
+	execute: async (client, interaction) => {
 
 		if (!interaction.isChatInputCommand()) return
 		const prompt = interaction.options.get('prompt', true).value as string
 
 
-	
+
 		const result = await model.generateContent(prompt);
 		const response = await result.response;
 
 		const text = response.text();
 
-		await silentMessage(interaction, `Prompt: ${prompt}`, text )
+		await silentMessage(interaction, `Prompt: ${prompt}`, text)
 
 	}
-}
-export default handler
+} as Command
