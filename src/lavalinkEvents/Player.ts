606842e0d3d3b411ae00df19afe18a242c5357d9
remove158 <remove158@gmail.com>
2024-02-02 04:20:14 +0700

    fix: disconnected

--- src/lavalinkEvents/Player.ts
+++ src/lavalinkEvents/Player.ts
@@ -2,6 +2,6 @@ import { BotClient } from "@/types/Client";
 
 export function PlayerEvents(client:BotClient) {
 	client.lavalink.on('queueEnd', (player) => {
-		player.destroy()
+		player.disconnect()
 	})
 }
