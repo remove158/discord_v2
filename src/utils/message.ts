c99f6fef3ff82d57a1259946aa2bdcebbcf05937
remove158 <remove158@gmail.com>
2024-02-03 03:05:25 +0700

    fix: embded color

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,6 +1,6 @@
 import { CacheType, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 
-export const builder =  (title : string , description : string) => new EmbedBuilder().setColor(0x625871).setTitle(title).setDescription(description)
+export const builder =  (title : string , description : string) => new EmbedBuilder().setColor(0xF29FBB).setTitle(title).setDescription(description)
 
 export const silentMessage = async( interaction : ChatInputCommandInteraction<CacheType> , title: string, description : string ) => {
 	return await interaction.reply({embeds:[builder(title,description)], flags: [4096] as any})
