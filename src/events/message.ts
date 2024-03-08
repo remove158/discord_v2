e77a2077757ee7fe73cf96494167d6cd66dcb743
remove158 <remove158@gmail.com>
2024-03-09 00:01:23 +0700

    fix: update command

--- src/events/message.ts
+++ src/events/message.ts
@@ -3,24 +3,23 @@ import { Event } from "@/types/Client";
 import { envConfig } from "@/config";
 
 export default {
-    name: Events.MessageCreate,
-    execute: async (client, message: Message) => {
-        if (
-            message.author.username === envConfig.dev.ownerId &&
-            message.content === "?setcommand"
-        ) {
-            try {
-                await client.application.commands.set(
-                    client.commands.map((v) =>
-                        v.data.toJSON()
-                    ) as ApplicationCommandDataResolvable[]
-                );
-                message.reply(
-                    `Loaded ${client.commands.size} slash (/) commands`
-                );
-            } catch {
-                message.reply(`Fail to load command`);
-            }
-        }
-    },
+	name: Events.MessageCreate,
+	execute: async (client, message: Message) => {
+		if (
+			message.content === "?setcommand"
+		) {
+			try {
+				await client.application.commands.set(
+					client.commands.map((v) =>
+						v.data.toJSON()
+					) as ApplicationCommandDataResolvable[]
+				);
+				message.reply(
+					`Loaded ${client.commands.size} slash (/) commands`
+				);
+			} catch {
+				message.reply(`Fail to load command`);
+			}
+		}
+	},
 } as Event;
