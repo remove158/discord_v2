4b96d935f68511bc09bac843b8498d1c63895366
remove158 <remove158@gmail.com>
2024-03-09 20:42:37 +0700

    update: client type

--- src/client.d.ts
+++ src/client.d.ts
@@ -1,14 +1,20 @@
 import type { Client, SlashCommandBuilder } from 'discord.js'
-import type { LavalinkManager } from 'lavalink-client/dist/types'
+import type { LavalinkManager } from 'lavalink-client'
 
 type InteractionExecuteFN = (
 	client: BotClient,
-	interaction: ChatInputCommandInteraction<CacheType>
-) => unknown
+	interaction: ChatInputCommandInteraction<'cached'>
+) => unknow
+
+type AutoCompleteExecuteFN = (
+	client: BotClient,
+	interaction: AutocompleteInteraction
+) => unknow
 
 type Command = {
 	data: SlashCommandBuilder
 	execute: InteractionExecuteFN
+	autocomplete?: subCommandAutocomplete
 }
 
 type BotClient = {
